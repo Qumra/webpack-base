618ec459f38029493f43e0e95c6d4d5ffc324d65
Qumra <39854812+Qumra@users.noreply.github.com>
2019-04-22 20:45:15 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,14 @@ module.exports = {
     module:{
         // 所有第三方模块的配置文件
         rules:[
-            {test:/|.js|jsx$/,use:'babel-loader',exclude:/node_modules/}  //一定要添加排除项exclude，否则项目起不来
+            {test:/\.js|jsx$/,use:'babel-loader',exclude:/node_modules/}  //一定要添加排除项exclude，否则项目起不来
         ]
+    },
+    resolve:{
+        extensions:['.js','.jsx','.json'], //表示这几个文件的后缀名可以不写
+        alias:{ //表示别名
+            '@':path.join(__dirname,'./src') //@表示项目目录中src的这一层目录
+        }
+
     }
 }
