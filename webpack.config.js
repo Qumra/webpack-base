5f2bbfe15673cf093ca451abaaa2df4e547d9e29
Qumra <39854812+Qumra@users.noreply.github.com>
2019-04-22 14:38:52 +0800

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+// webpack的配置文件
+// webpack默认只能打包.js后缀名类型的文件，像.png .vue无法主动配置，所以需要配置第三方的loder
+// html-webpack-plugin可将html文件打包在内存中
+const path = require('path');
+const HtmlWebpackplugin = require('html-webpack-plugin');
+const htmlWebpackplugin =new HtmlWebpackplugin({
+    template:path.join(__dirname,'src/index.html'),
+    filename:'index.html'
+})
+module.exports = {
+    mode:"development",
+    plugins:[
+        htmlWebpackplugin
+    ],
+    module:{
+        // 所有第三方模块的配置文件
+        rules:[
+            {test:/|.js|jsx$/,use:'babel-loader',exclude:/node_modules/}  //一定要添加排除项exclude，否则项目起不来
+        ]
+    }
+}
