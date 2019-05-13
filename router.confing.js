97cccf5a8c7f69ef718f5c3c7930b2a0521a5c8d
Qumra <39854812+Qumra@users.noreply.github.com>
2019-05-13 17:43:56 +0800

    Update router.confing.js

--- router.confing.js
+++ router.confing.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Router, Route, Switch, browserHistory, /* , Redirect */ } from 'react-router-dom'
+import {HashRouter , Route, Switch, browserHistory, /* , Redirect */ } from 'react-router-dom'
 import history from './history';
 import * as base from '@/views/base'
 import app from '@/App'
 export default () => (
     //设为hashHistory，路由将通过URL的hash部分（#）切换，URL的形式类似example.com/#/some/path。
-    <Router history={history}>
+    <HashRouter history={history}>
         <Switch>
-            <Route path="/" component={app}>
-                <Route path="/login" component={base.Login} />
-                <Route path="/main" component={base.Main} />
+            <Route path="/" component={app} />
+            {/* <Route path="/login" component={base.Login} /> */}
+            <Route path="/main" component={base.Main}>
             </Route>
         </Switch>
-    </Router>
+    </HashRouter>
 )
